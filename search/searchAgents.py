0ce9fe341656056fa44be528b8e48b5591437b83
yasha424 <jakob20022404@gmail.com>
2022-09-27 19:20:38 +0300

    complete cornerProblem (q5)

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -292,6 +292,7 @@ class CornersProblem(search.SearchProblem):
         # Please add any code here which you would like to use
         # in initializing the problem
         "*** YOUR CODE HERE ***"
+        self.initialState = [0, 0, 0, 0]
 
     def getStartState(self):
         """
@@ -299,14 +300,18 @@ class CornersProblem(search.SearchProblem):
         space)
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return (self.startingPosition, self.initialState)
 
     def isGoalState(self, state: Any):
         """
         Returns whether this search state is a goal state of the problem.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        for corner in state[1]:
+            if corner == 0:
+                return False
+
+        return True
 
     def getSuccessors(self, state: Any):
         """
@@ -329,6 +334,17 @@ class CornersProblem(search.SearchProblem):
             #   hitsWall = self.walls[nextx][nexty]
 
             "*** YOUR CODE HERE ***"
+            x, y = state[0]
+            corners = state[1].copy()
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+
+            if not self.walls[nextx][nexty]:
+                if (nextx, nexty) in self.corners:
+                    corners[self.corners.index((nextx, nexty))] = 1
+
+                nextState = ((nextx, nexty), corners)
+                successors.append((nextState, action, 1))
 
         self._expanded += 1 # DO NOT CHANGE
         return successors
